c97058b00d95677ea90d845f56345377698393ee
piForever <pilarbenito98@hotmail.com>
2021-10-27 20:10:57 -0300

    working on the base logic

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const words = new URLSearchParams(window.location.search);
-
-const score1 = words.get('score1');
-var lowSociability = "" ; 
-var mediumSociability = "" ; 
-var highSocialibility = "" ; 
-
 const sociability = [`sociability low aspects` , "sociability medium aspects", "sociability high aspects"]
-function sociabilityInformation(score1) {
-if (score1=== "low") {
+const sociability = document.querySelectorAll('input[name="score1"]');
+let selectedValue;
+var lowSociability  ; 
+var mediumSociability ; 
+var highSocialibility ; 
+
+function sociabilityInformation(selectedValue) {
+if (selectedValue=== "low") {
      lowSociability = sociability[0];
-}   else if (score1==="medium") {
+}   else if (selectedValue==="medium") {
      mediumSociability = sociability[1];
 } else {
      highSocialibility = sociability[2];
@@ -17,9 +16,29 @@ if (score1=== "low") {
 }
 }
 
+for (const rb1 of sociability) {
+    if (rb1.checked) {
+        selectedValue = rb1.value;
+        break;
+    }
+}
+
+sociabilityInformation(selectedValue);
+
+
+// otra manera de chequear el seleccionado 
+/* if(document.getElementById('low').checked) {
+
+}else if(document.getElementById('medium').checked) {
+
+}else {
+ ;
+
+} 
+*/
+
 
 let liderazgo = document.getElementById('liderazgo');
 liderazgo.innerHTML = `<span>${mediumSociability}</span>`;
 
-
-``
+    
